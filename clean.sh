640ff85929d2245e15196f29fe5108aaeaa77f80
xgqfrms <xgqfrms@outlook.com>
2021-03-21 18:14:13 +0800

    update

--- clean.sh
+++ clean.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
 # echo "^-v-^ cleaning files ..." && yarn clean
-echo "^-v-^ cleaning files ..." && rm -rf ./docs/*.*
+echo "^-v-^ cleaning files ..." && rm -rf ./docs/*
+# no yes/no ???
+# echo "^-v-^ cleaning files ..." && rm -Rf ./docs/*
