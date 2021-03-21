eca38da1664ef0cd11f59228442b9862c0c35f4b
xgqfrms <xgqfrms@outlook.com>
2021-03-21 17:06:52 +0800

    npm run develop

--- website/gatsby-config.js
+++ website/gatsby-config.js
@@ -1,5 +1,24 @@
 module.exports = {
-    plugins: [
-
-    ]
-}
+  siteMetadata: {
+    title: "website",
+  },
+  plugins: [
+    "gatsby-plugin-styled-components",
+    "gatsby-plugin-sitemap",
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        icon: "src/images/icon.png",
+      },
+    },
+    "gatsby-plugin-mdx",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "pages",
+        path: "./src/pages/",
+      },
+      __key: "pages",
+    },
+  ],
+};
