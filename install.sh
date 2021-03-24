e100b3b80db864c2864f84b3c357ca514b1a815f
xgqfrms <xgqfrms@outlook.com>
2021-03-25 00:03:56 +0800

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# 
+# @author xgqfrms
+# @license MIT
+# @copyright xgqfrms
+# @created 2020-01-0
+# 
+# @description 
+# @augments 
+# @example 
+# @link 
+# 
+
+# echo "^-v-^ app is installing ..." && yarn
+# echo "^-v-^ app is installing ..." && cd ./website && yarn
+# echo "^-v-^ app is installing ..." && cd ./website && npm i
+echo "^-v-^ app is installing ..." && cd ./website && npm install
+
