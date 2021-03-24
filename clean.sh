225273d9a52d0dc35cacc0997ccf03c1fa932e5b
xgqfrms <xgqfrms@outlook.com>
2021-03-25 00:42:29 +0800

    update clean

--- clean.sh
+++ clean.sh
@@ -5,7 +5,20 @@
 # no yes/no ??? bug
 # echo "^-v-^ cleaning files ..." && rm -Rf ./docs/*
 
-echo "^-v-^ cleaning files ..." && rimraf ./docs
+# delete docs folder
+echo "^-v-^ cleaning files ..." && ./node_modules/rimraf/bin.js ./docs
+# echo "^-v-^ cleaning files ..." && ./node_modules/rimraf/bin.js ./docs/
+
+# delete all folders and files in docs folder
+# echo "^-v-^ cleaning files ..." && ./node_modules/rimraf/bin.js ./docs/*
+
+# only delete all files in docs folder
+# echo "^-v-^ cleaning files ..." && ./node_modules/rimraf/bin.js ./docs/*.*
+
+# only delete all files in docs folder & sub-folders
+# echo "^-v-^ cleaning files ..." && ./node_modules/rimraf/bin.js ./docs/**/*.*
+
+# echo "^-v-^ cleaning files ..." && rimraf ./docs
 # echo "^-v-^ cleaning files ..." && rimraf ./docs/*.*
 # echo "^-v-^ cleaning files ..." && rimraf ./docs/**/*.*
 
--- copy.sh
+++ copy.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
 # echo "^-v-^ copying files ..." && yarn copy
+echo "^-v-^ create folder ..." && mkdir docs
 echo "^-v-^ copying CNAME ..." && cp ./CNAME ./docs
 echo "^-v-^ copying files ..." && cp -r ./website/public/* ./docs
